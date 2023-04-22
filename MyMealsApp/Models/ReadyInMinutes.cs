3b327263061a2f010ac60f686c183968c7ee1333
gerasoa <gerasoa@gmail.com>
2023-04-22 10:42:17 +0100

    10 recipes. class ReadyInMinutes created for future

---
+++ MyMealsApp/Models/ReadyInMinutes.cs
@@ -0,0 +1,9 @@
+﻿namespace MyMealsApp.Models
+{
+    public class ReadyInMinutes
+    {
+        public int ID { get; set; }
+        public int Prep { get; set; }
+        public int Cook { get; set; }
+    }
+}
