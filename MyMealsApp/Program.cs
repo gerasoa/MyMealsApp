f1c8449283c5a0f59660b2dc0c606f596e959240
gerasoa <gerasoa@gmail.com>
2023-04-29 11:48:40 +0100

    Add CORS / UseStaticFiles / HttpContextAccessor

--- MyMealsApp/Program.cs
+++ MyMealsApp/Program.cs
@@ -1,6 +1,7 @@
 using MyMealsApp.Context;
 using MyMealsApp.Repository;
 using MyMealsApp.Contract;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,9 +26,23 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseCors(c =>
+    {
+        c.AllowAnyHeader();
+        c.AllowAnyMethod();
+        c.AllowAnyOrigin();
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "images")),
+    RequestPath = "/images"
+});
 
 app.UseAuthorization();
 
